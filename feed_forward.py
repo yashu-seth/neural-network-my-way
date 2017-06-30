a118b1bf2ed43c0572ecbdec4b36c2dafa284201
Yashu Seth <yashuseth2503@gmail.com>
2017-06-30 22:52:20 +0530

    added forward prop, fixed random_initialize

--- feed_forward.py
+++ feed_forward.py
@@ -1,12 +1,13 @@
 import numpy as np
 
 class FeedForward():
-    def __init__(self, data, labels, no_of_output_units = 1, no_of_hidden_layers = 1, no_of_hidden_units = None):
+    def __init__(self, data, labels, no_of_output_units, no_of_hidden_layers = 1, no_of_hidden_units = None):
 
         self.X = data
         self.no_of_samples = self.X.shape[0]
         self.no_of_input_units = self.X.shape[1]
         self.Y = labels
+        self.t = np.zeros(shape=(self.Y.shape[0], 1))
 
         if self.no_of_samples != self.Y.shape[0] or self.Y.shape[1] != 1:
             raise ValueError
@@ -36,9 +37,22 @@ class FeedForward():
         self.weights.extend([np.random.uniform(size=(self.no_of_hidden_units[i-1], self.no_of_hidden_units[i]))
                             for i in range(1, self.no_of_hidden_layers)])
         #weight matrix between output layer and last hidden layer
-        self.weights.append(np.random.uniform(size=(self.no_of_hidden_units[self.no_of_hidden_layers - 1], self.no_of_output_units)))
+        self.weights.append(np.random.uniform(size=(self.no_of_hidden_units[self.no_of_hidden_layers - 1],
+                                                    self.no_of_output_units)))
 
-        self.biases = [np.random.uniform(self.no_of_hidden_units[i]) for i in range(self.no_of_hidden_layers)]
-        self.biases.append([np.random.uniform(self.no_of_output_units)])
+        self.biases = [np.random.uniform(size=(1, self.no_of_hidden_units[i])) for i in range(self.no_of_hidden_layers)]
+        self.biases.append(np.random.uniform(size=(1, self.no_of_output_units)))
 
-    
+    def forward_prop(self):
+        if self.weights == None:
+            self.random_initialize()
+
+        current_input = self.X
+
+        for i in range(self.no_of_hidden_layers + 1):
+            current_input = self.sigmoid(np.dot(current_input, self.weights[i]) + self.biases[i])
+
+        self.t = current_input
+
+    def backward_prop(self):
+        pass
